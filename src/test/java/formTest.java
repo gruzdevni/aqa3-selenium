c83badbe9a4c3cff485b40be759a037cbda12186
Nikolay Gruzdev <gruzdevni@mail.ru>
2019-10-27 21:07:27 +0300

    Initial commit - ready for test design

---
+++ src/test/java/formTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class formTest {
+
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+// убедитесь, что файл chromedriver.exe расположен именно в каталоге C:\tmp
+        System.setProperty("webdriver.chrome.driver", "C:\\aqa\\3\\driver\\chromedriver.exe");
+    }
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldTestSomething() {
+        driver.get("http://localhost:9999");
+    }
+
+}
